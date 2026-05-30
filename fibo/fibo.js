d531e88eab647a88184e24cd173cdd70e14e3fa7
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-05-30 16:21:04 -0300

    feat: iterative fibo
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

--- fibo/fibo.js
+++ fibo/fibo.js
@@ -1,11 +1,23 @@
-function fibs(a) {
-  if (typeof a != "number" || isNaN(a)) {
+function fibs(n) {
+  if (typeof n != "number" || isNaN(n)) {
     return "error";
   }
-  if (a <= 0 || a / 1 != 0) {
+  if (n <= 0 || n % 1 != 0) {
     return "natural numbers only!";
   }
-  return 1;
+
+  let ans = [0];
+
+  if (n == 1) {
+    return ans;
+  }
+  ans.push(1);
+
+  for (let i = 2; i < n; ++i) {
+    ans.push(ans[i - 1] + ans[i - 2]);
+  }
+
+  return ans;
 }
 
 export default fibs;
--- fibo/fibo.test.js
+++ fibo/fibo.test.js
@@ -14,3 +14,9 @@ test("invalid inputs", () => {
   expect(fibs(-1)).toBe("natural numbers only!");
   expect(fibs(1.23)).toBe("natural numbers only!");
 });
+
+test("valid inputs", () => {
+  expect(fibs(1)).toEqual([0]);
+  expect(fibs(2)).toEqual([0, 1]);
+  expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
