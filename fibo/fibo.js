bcd102c41dff827a8f501aed260650035f23267a
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-05-30 16:38:59 -0300

    feat: recursive fibo
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

--- fibo/fibo.js
+++ fibo/fibo.js
@@ -20,4 +20,26 @@ function fibs(n) {
   return ans;
 }
 
-export default fibs;
+function recFibs(n) {
+  if (typeof n != "number" || isNaN(n)) {
+    return "error";
+  }
+  if (n <= 0 || n % 1 != 0) {
+    return "natural numbers only!";
+  }
+
+  return rec(n, [], 0);
+}
+
+function rec(n, l, i) {
+  if (n == 1) {
+    return [0];
+  }
+  if (n == 2) {
+    return [0, 1];
+  }
+  const prev = rec(n - 1, l, i + 1);
+  return [...prev, prev.at(-1) + prev.at(-2)];
+}
+
+export { fibs, recFibs };
--- fibo/fibo.test.js
+++ fibo/fibo.test.js
@@ -1,4 +1,4 @@
-import fibs from "./fibo.js";
+import { fibs, recFibs } from "./fibo.js";
 
 test("function exists", () => {
   expect(fibs()).not.toBe();
@@ -19,4 +19,25 @@ test("valid inputs", () => {
   expect(fibs(1)).toEqual([0]);
   expect(fibs(2)).toEqual([0, 1]);
   expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
+
+test("(recursive) function exists", () => {
+  expect(recFibs()).not.toBe();
+});
+
+test("(recursive) invalid inputs", () => {
+  expect(recFibs([1, 2])).toBe("error");
+  expect(recFibs({ a: "b" })).toBe("error");
+  expect(recFibs("a")).toBe("error");
+  expect(recFibs(null)).toBe("error");
+
+  expect(recFibs(0)).toBe("natural numbers only!");
+  expect(recFibs(-1)).toBe("natural numbers only!");
+  expect(recFibs(1.23)).toBe("natural numbers only!");
+});
+
+test("(recursive) valid inputs", () => {
+  expect(recFibs(1)).toEqual([0]);
+  expect(recFibs(2)).toEqual([0, 1]);
+  expect(recFibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
 });
