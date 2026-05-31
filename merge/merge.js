e5e712408db06a64b92724da491a3e1ab58b2362
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-05-31 13:43:25 -0300

    feat: valid merge function + simple tests
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

--- merge/merge.js
+++ merge/merge.js
@@ -2,10 +2,56 @@ function merge(v) {
   if (!Array.isArray(v)) {
     return "error";
   }
-  if (v.length <= 0) {
-    return "error"
+  if (v.length < 0) {
+    return "error";
   }
-  return 1;
+  if (v.length == 1 || v.length == 0) {
+    return v;
+  }
+
+  let [a, b] = recSplit(v);
+  a = merge(a);
+  b = merge(b);
+  return recSort(a, b);
+}
+
+function recSplit(v) {
+  const half = v.length / 2;
+  let l = [];
+  let r = [];
+  for (let i = 0; i < v.length; ++i) {
+    if (i < half) {
+      l.push(v[i]);
+    } else {
+      r.push(v[i]);
+    }
+  }
+  return [l, r];
+}
+
+function recSort(a, b) {
+  const out = [];
+  let [i, j] = [0, 0];
+  while (i < a.length && j < b.length) {
+    if (!b || a[i] <= b[j]) {
+      out.push(a[i]);
+      ++i;
+    } else if (!a || a[i] > b[j]) {
+      out.push(b[j]);
+      ++j;
+    }
+  }
+
+  while (i < a.length) {
+    out.push(a[i]);
+    ++i;
+  }
+
+  while (j < b.length) {
+    out.push(b[j]);
+    ++j;
+  }
+  return out;
 }
 
 export default merge;
--- merge/merge.test.js
+++ merge/merge.test.js
@@ -8,5 +8,12 @@ test("invalid inputs", () => {
   expect(merge("a")).toBe("error");
   expect(merge(1)).toBe("error");
   expect(merge({ a: "b" })).toBe("error");
-  expect(merge([])).toBe("error");
+});
+
+test("valid inputs", () => {
+  expect(merge([])).toEqual([]);
+  expect(merge([1])).toEqual([1]);
+  expect(merge([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+  expect(merge([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  expect(merge([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
 });
